decce47e4948c3839a95ae4bbef4a670eb95bbf5
fjnkt98 <50233866+fjnkt98@users.noreply.github.com>
2021-08-23 21:59:23 +0900

    Add example of Get-Content command-let

---
+++ fundamentals/practice6.ps1
@@ -0,0 +1,9 @@
+﻿###########################
+# Training of Get-Content #
+###########################
+
+$lines = Get-Content -Path .\practice6.txt
+
+foreach ($items in $lines) {
+  Write-Host $items
+}
